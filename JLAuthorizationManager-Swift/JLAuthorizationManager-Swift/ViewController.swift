2b076a9c053f3a78cd8d10d6d5a40005d5fe4379
zhangdi <809184491@qq.com>
2019-01-09 11:08:33 +0800

    init project

---
+++ JLAuthorizationManager-Swift/JLAuthorizationManager-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JLAuthorizationManager-Swift
+//
+//  Created by zhangdi on 2019/1/9.
+//  Copyright © 2019年 Jacklin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
